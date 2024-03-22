2fa104499eaa7f9e1d98699c9b2398e7a9ff415d
Vasily_Romanov <myhappy1988@gmail.com>
2024-03-22 21:01:11 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.sass';
+import Framepost from './components/Framepost/Framepost'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
