8f29def599e8ddc18565be79ed08e2673bd01493
Vasya1988 <myhappy1988@gmail.com>
2024-03-31 12:48:09 +0300

    add comments

--- src/App.js
+++ src/App.js
@@ -5,63 +5,63 @@ import Framepost from './components/Framepost/Framepost'
 
 function App() {
 
+  // Массив в который добавляются новости
   const [posts, setPosts] = useState([])
+
+  // Счетчик подгружаемых новостей
   const [pageCounter, setPageCounter] = useState(1)
+
+  // Функция запрос API
   const responseApi = (page=1) => {
-  
     fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`)
       .then(response => response.json())
       .then(res => setPosts([...posts, ...res]))
       // .then(res => setPosts(res))
   }
 
-  const newsUpdate = (scrollChange) => {
-    // Common page height
-    const height = scrollChange.target.documentElement.scrollHeight
+  // Функция отслеживания положение скролла
+  const scrollPosition = (scroll) => {
+    
+    const height = scroll.target.documentElement.scrollHeight
 
-    // Current scroll position
-    const scrollCurrent = scrollChange.target.documentElement.scrollTop
+    const scrollCurrent = scroll.target.documentElement.scrollTop
 
-    // Visible of page height
     const visiblePageHeight = window.innerHeight
 
+    // Остановка добавления новостей по скроллу, после 5 подгрузок
     if (pageCounter < 5) {
       if (scrollCurrent + visiblePageHeight === height) {
         setPageCounter(pageCounter + 1)
       }
     }
-
-    
   }
 
   useEffect(() => {
-    console.log(pageCounter)
-    console.log(posts)
+    // Запрос новостей к api
     responseApi(pageCounter)
-    window.addEventListener('scroll', newsUpdate)
+
+    // Добавление события сролл
+    window.addEventListener('scroll', scrollPosition)
 
     return () => {
-      window.removeEventListener('scroll', newsUpdate)
+      window.removeEventListener('scroll', scrollPosition)
     }
-  }, [pageCounter])
+  }, [pageCounter]) // Запускаем useEffect каждый раз после обновления pageCounter
 
   return (
     <div className="App">
 
       <Framepost posts={posts} />
       {
+        // Добавляем кнопку на страницу
         pageCounter >= 5 
-          ? <button
-            onClick={() => {
-              setPageCounter(pageCounter + 1)
-              console.log(pageCounter)
-            }}
-          >
-            Загрузить еще
-          </button>
+          ? <button onClick={() => {
+                setPageCounter(pageCounter + 1)}}
+            >
+              Загрузить еще
+            </button>
           : false
       }
-      
     </div>
   );
 }
